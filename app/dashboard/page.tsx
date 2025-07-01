49f25faea2efcc32a00b283309f0b87ac2cc13d6
Daniela Londoño <daniela.londono@swissmedical.com.ar>
2025-07-01 15:54:30 -0300

    feat: :sparkles: see data & insert

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { fetchCustomers, fetchLatestInvoices, fetchRevenue } from "../lib/data"
+import { fetchCardData, fetchCustomers, fetchLatestInvoices, fetchRevenue } from "../lib/data"
 import { Card } from "../ui/dashboard/cards";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import RevenueChart from "../ui/dashboard/revenue-chart";
@@ -7,6 +7,7 @@ import { lusitana } from "../ui/fonts";
 import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "../ui/skeletons";
 
 export  default async function page () {
+  const {numberOfCustomers, numberOfInvoices, totalPaidInvoices,totalPendingInvoices} = await fetchCardData()
   
   return (
     <main>
@@ -14,14 +15,14 @@ export  default async function page () {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> */}
+        />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
